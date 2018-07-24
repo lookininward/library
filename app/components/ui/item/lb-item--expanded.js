9ebf04b916d6d861a91a71364e60dd5f03b0bffc
lookininward <vinoth.michaelxavier@gmail.com>
2018-07-24 15:36:07 -0400

    Update component names, editing mode for item expanded view

---
+++ app/components/ui/item/lb-item--expanded.js
@@ -0,0 +1,35 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  isEditing: false,
+
+  itemData: computed('item', function() {
+    return {
+      'title' : this.get('item.title'),
+      'subtitle' : this.get('item.subtitle'),
+      'author' : this.get('item.author'),
+      'description' : this.get('item.description')
+    }
+  }),
+
+  actions: {
+    toggleEdit() {
+      this.toggleProperty('isEditing');
+    },
+
+    saveChanges(form) {
+      const item = this.get('item');
+      const itemData = this.get('itemData');
+
+      item.set('title', itemData.title);
+      item.save();
+    },
+
+    readItem() {
+      const item = this.get('item');
+      console.log('Reading item...');
+      // set as current on library controller
+    }
+  }
+});
