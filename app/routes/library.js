22c7956aa14576ce74af08a79ae8f8c8d9b79337
lookininward <vinoth.michaelxavier@gmail.com>
2018-07-24 10:29:19 -0400

    Show item cover thumbnails

--- app/routes/library.js
+++ app/routes/library.js
@@ -7,13 +7,27 @@ export default Route.extend({
         'id': 1,
         'title' : 'Conquistador',
         'subtitle' : 'Hernán Cortés, King Montezuma, and the Last Stand of the Aztecs',
-        'author' : 'Buddy Levy'
+        'author' : 'Buddy Levy',
+        'cover' : 'conquistador.jpg'
       },
       {
         'id': 2,
         'title' : 'East of Eden',
         'author' : 'John Steinbeck'
-      }
+      },
+      {
+        'id': 3,
+        'title' : 'The Wise Man\'s Fear',
+        'author' : 'Patrick Rothfuss',
+        'cover' : 'fear.jpg'
+      },
+      {
+        'id': 4,
+        'title' : 'Sex at Dawn',
+        'subtitle' : 'The Prehistoric Origins of Modern Sexuality',
+        'author' : 'Christoper Ryan, Cacilda Jethá',
+        'cover' : 'dawn.jpg'
+      },
     ]
   }
 });
